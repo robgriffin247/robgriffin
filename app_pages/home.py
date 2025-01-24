148bfb3ff3356d9c0996d2d9eeecc1000748d278
robgriffin247 <robgriffin247@hotmail.com>
2025-01-24 17:25:47 +0100

    extra push

--- app_pages/home.py
+++ app_pages/home.py
@@ -11,11 +11,11 @@ main_body = '''
 This site has been created to host and showcase a number of smaller web-apps &mdash; 
 apps where a stand-alone app would be overkill &mdash; while larger apps have dedicated deployments.
 These smaller apps have largely been created as a way to practice coding, try new 
-technologies or yest new ideas &mdash; even this site was partly to test making multipage apps in Streamlit.       
+technologies or test new ideas &mdash; even this site was partly to test making multipage apps in Streamlit.       
 
 I am both a data nerd and keen cyclist &mdash; spending a lot of time riding and racing on the virtual roads 
 of Zwift &mdash; so a lot of my projects combine these two worlds. Check out some of my larger projects below
-and smaller apps in the navigation menu.'''
+and smaller apps in the navigation menu (← over there!)'''
 
 projects = [
     {'display':False, 'title':'RouteViewer', 'link':'https://routeviewer.streamlit.app', 'description':'A platform providing race notes for routes on Zwift, built on a database of road descriptions and route landmarks that combine to create consistent notes for the various routes that pass through the same roads.'},
