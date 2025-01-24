eab540733370785b07051beb3b2cd07e74a939a1
robgriffin247 <robgriffin247@hotmail.com>
2025-01-24 09:28:09 +0100

    added about info to ladder

--- app_pages/zwift/ladder_score.py
+++ app_pages/zwift/ladder_score.py
@@ -19,3 +19,16 @@ for position in range(1,n_riders+1):
 
 st.code(f"{'Win :D' if total>(max_points-total) else 'Loss :(' if total<(max_points-total) else 'Draw :/'}")
 st.code(f"Scored {total} of {max_points} points")
+
+st.divider()
+
+st.markdown('''
+**About**
+            
+The Club Ladder is a racing format and league in Zwift. 
+Races feature two teams, of up to 5 riders each, going head-to-head. 
+Points are awarded as 10 for 1st, 9 for 2nd ... 1 for 10th. 
+The team with the most points wins. 
+I built this tool to allow a quick input of rider positions during races to
+make it easier to judge the situation on the road and help guide strategy.
+            ''')
